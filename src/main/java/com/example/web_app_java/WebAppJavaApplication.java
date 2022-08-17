513acffa849f188c5d875b38fcc5dab115d510ac
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-08-17 11:45:33 +0200

    Initial commit

---
+++ src/main/java/com/example/web_app_java/WebAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_app_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppJavaApplication.class, args);
+	}
+
+}
