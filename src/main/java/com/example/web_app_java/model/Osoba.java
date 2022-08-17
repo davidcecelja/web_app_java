146bb09d95a518c8d2c287b551648c9dcf55252f
davidcecelja <46240796+davidcecelja@users.noreply.github.com>
2022-08-17 13:35:11 +0200

    Osoba class add

---
+++ src/main/java/com/example/web_app_java/model/Osoba.java
@@ -0,0 +1,21 @@
+package com.example.web_app_java.model;
+
+import java.util.UUID;
+
+public class Osoba {
+    private final UUID id;
+    private final String ime;
+
+    public Osoba(UUID id, String ime) {
+        this.id = id;
+        this.ime = ime;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+}
